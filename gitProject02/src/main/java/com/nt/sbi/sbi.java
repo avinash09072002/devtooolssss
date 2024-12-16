fc316a14de7e622fff83e6202c65b7ae2b51fcc4
Avinash Dharmshale <avinashdh2000@gmail.com>
2024-12-16 13:35:55 +0530

    intial commit

---
+++ gitProject02/src/main/java/com/nt/sbi/sbi.java
@@ -0,0 +1,5 @@
+package com.nt.sbi;
+
+public class sbi {
+
+}
---
+++ gitProject02/src/main/java/nt/gitProject02/App.java
@@ -0,0 +1,10 @@
+package nt.gitProject02;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello Wdforld!");
+    }
+}
